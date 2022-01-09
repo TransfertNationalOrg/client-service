670b83b464b9c69a6e95d653b30d688a1d385ad1
ambou-97 <ambroise.tine@gmail.com>
2022-01-10 00:57:47 +0100

    model

---
+++ src/main/java/ma/ensa/model/Client.java
@@ -0,0 +1,38 @@
+package ma.ensa.model;
+
+import lombok.Data;
+import ma.ensa.model.enumer.IDENTITE;
+import ma.ensa.model.enumer.PAYS;
+import ma.ensa.model.enumer.TITRE;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Data
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private TITRE titre;
+    private String prenom;
+    private String nom;
+    private IDENTITE identite;
+    private PAYS pays;
+    private Long numIdentite;
+    private Date expiration;
+    private Date naissance;
+    private String profession;
+    private PAYS nationalite;
+    private String adressePays;
+    private String adresseLegale;
+    private String ville;
+    private Long gsm;
+    private String email;
+    private String matricule;
+
+}
