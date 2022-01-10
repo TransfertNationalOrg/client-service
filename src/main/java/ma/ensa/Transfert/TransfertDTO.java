49aba42e469ddb1ff49b875aac9f30778e36256a
ambou-97 <ambroise.tine@gmail.com>
2022-01-10 17:57:48 +0100

    TransfertDTO

---
+++ src/main/java/ma/ensa/Transfert/TransfertDTO.java
@@ -0,0 +1,9 @@
+package ma.ensa.Transfert;
+
+public class TransfertDTO {
+    private Long id;
+    private double montant;
+    private Long ref;
+    private double commission;
+    private String motif;
+}
